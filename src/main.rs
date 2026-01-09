a0984f16a5a8c8d8196464410c2aed49de4de7b3
ayush <ayushagarwal.dev@gmail.com>
2026-01-09 20:08:08 +0530

    add: AsyncSleep struct impl Future

--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,32 @@ where
     task
 }
 
+struct AsyncSleep {
+    start_time: Instant,
+    duration: Duration,
+}
+
+impl AsyncSleep {
+    fn new(duration: Duration) -> Self {
+        Self {
+            start_time: Instant::now(),
+            duration,
+        }
+    }
+}
+
+impl Future for AsyncSleep {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let elapsed_time = self.start_time.elapsed();
+        if elapsed_time >= self.duration {
+            Poll::Ready(())
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
 fn main() {}
